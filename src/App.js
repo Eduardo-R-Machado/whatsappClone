095e5f94a6b91bc842e8e564e6643e7d50379c82
eduardo329 <eduardo_rj_18@hotmail.com>
2021-11-22 18:12:21 -0300

    preparando layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className="app-window">
+      <div className="sidebar">
+        <header>
+          <img className="header-avatar" src='https://img.freepik.com/vetores-gratis/avatar-de-personagem-de-empresario-isolado_24877-60111.jpg?size=338&ext=jpg' alt='' />
+          <div className="header-buttons">
+            <div className="header-btn">
+              <DonutLargeIcon style={{ color: '#919191' }} />
+            </div>
+            <div className="header-btn">
+              <ChatIcon style={{ color: '#919191' }} />
+            </div>
+            <div className="header-btn">
+              <MoreVertIcon style={{ color: '#919191' }} />
+            </div>
+          </div>
+        </header>
+
+        <div className="search">
+          ...
+        </div>
+
+        <div className="chatList">
+          ...
+        </div>
+
+      </div>
+
+      <div className="content-area">
+        ...
+      </div>
     </div>
   );
 }
-
-export default App;
